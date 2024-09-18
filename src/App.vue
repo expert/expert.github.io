c1fe74deac79dd9a4110eb41c38827325f398618
alex <fktjif@gmail.com>
2024-09-18 18:48:26 +0300

    [Build]

--- src/App.vue
+++ src/App.vue
@@ -4,12 +4,10 @@ import { Button } from '@/components/ui/button'
 import {
   NavigationMenu,
   NavigationMenuContent,
-  NavigationMenuIndicator,
   NavigationMenuItem,
   NavigationMenuLink,
   NavigationMenuList,
   NavigationMenuTrigger,
-  NavigationMenuViewport,
 } from "@/components/ui/navigation-menu"
 
 </script>
